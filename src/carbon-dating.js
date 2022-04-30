3b175b4a2f31989d4209721491ec572c89eb10fb
vzakharenkova <v.s.zakharenkova@gmail.com>
2022-04-30 16:13:45 +0300

    Carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,11 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (Number(sampleActivity) > 0 && Number(sampleActivity) < 5731 && Math.floor(Number(sampleActivity)) !== 15 && typeof sampleActivity === 'string' ) {
+    let t = Math.log(MODERN_ACTIVITY/sampleActivity)/(0.693/HALF_LIFE_PERIOD);
+    return Math.ceil(t);
+  } else return false
 }
 
 module.exports = {

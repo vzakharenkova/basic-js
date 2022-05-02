70bc056fd2f8885009ba334bb3701af254681d23
vzakharenkova <v.s.zakharenkova@gmail.com>
2022-05-02 23:00:09 +0300

    VigenereCipheringMachine

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error ('Incorrect arguments!')
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error ('Incorrect arguments!')
+    }
   }
 }
 
